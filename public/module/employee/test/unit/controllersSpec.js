236422f5bb3769deffec0263d02eed93fb3bdf3d
bmehler <bernhard.mehler@gmail.com>
2015-08-16 12:20:07 +0000

    changed style

--- public/module/employee/test/unit/controllersSpec.js
+++ public/module/employee/test/unit/controllersSpec.js
@@ -1,81 +1,78 @@
 describe('Unit testing controllers', function () {
 
-    beforeEach(module('application'));
+  beforeEach(module('application'));
 
-    var controller;
+  var controller;
 
-    it('should have an EmployeeListCtrl controller', function () {
-        expect(EmployeeListCtrl).toBeDefined();
-    });
+  it('should have an EmployeeListCtrl controller', function () {
+    expect(EmployeeListCtrl).toBeDefined();
+  });
 
-    it('should have a property orderProp which is name',
-        inject( function($controller, $rootScope) {
-            scope = $rootScope.$new();
+  it('should have a property orderProp which is name',
+    inject(function($controller, $rootScope) {
+      scope = $rootScope.$new();
 
-            controller = $controller('EmployeeListCtrl', {
-                $scope: scope
-            });
+      controller = $controller('EmployeeListCtrl', {
+        $scope: scope
+      });
 
-            expect(controller.orderProp).toBe('name');
-        })
-    );
-
-    it('should fetch list of employees',
-        inject(function($controller, $rootScope, _$httpBackend_) {
-
-          scope = $rootScope.$new();
-          httpBackend = _$httpBackend_;
-
-          httpBackend.when('GET', 'data/storage.json')
-              .respond(
-                    { name: 'Daisy Dougles' }
-              );
-
-          controller = $controller('EmployeeListCtrl', {
-              $scope: scope
-          });
-
-          httpBackend.flush();
-
-          expect(controller.employees.name).toBeDefined();
-          expect(controller.employees).toEqual({ name: 'Daisy Dougles' });
-          expect(controller.employees.name.length).toBe(13);
-      })
+      expect(controller.orderProp).toBe('name');
+    })
   );
 
-    it('should have an EmployeeDetailCtrl controller', function () {
-        expect(EmployeeDetailCtrl).toBeDefined();
-    });
+  it('should fetch list of employees',
+    inject(function($controller, $rootScope, _$httpBackend_) {
+      scope = $rootScope.$new();
+      httpBackend = _$httpBackend_;
 
-    it('should have correct css class',
-        inject( function($controller, $rootScope) {
+      httpBackend.when('GET', 'data/storage.json')
+        .respond(
+          { name: 'Daisy Dougles' }
+        );
 
-            scope = $rootScope.$new();
+      controller = $controller('EmployeeListCtrl', {
+        $scope: scope
+      });
 
-            controller = $controller('EmployeeDetailCtrl', {
-                $scope: scope
-            });
+      httpBackend.flush();
 
-           expect(scope.classChange(24)).toBe('progress-bar progress-bar-danger');
-           expect(scope.classChange(74)).toBe('progress-bar progress-bar-warning');
-           expect(scope.classChange(75)).toBe('progress-bar progress-bar-success');
-           expect(scope.classChange(24)).not.toBe('progress-bar progress-bar-success');
-           expect(scope.classChange(74)).not.toBe('progress-bar progress-bar-success');
-           expect(scope.classChange(75)).not.toBe('progress-bar progress-bar-danger');
-        })
-    );
+      expect(controller.employees.name).toBeDefined();
+      expect(controller.employees).toEqual({ name: 'Daisy Dougles' });
+      expect(controller.employees.name.length).toBe(13);
+    })
+  );
 
-    it('should take id as specified',
-        inject( function($controller, $rootScope) {
+  it('should have an EmployeeDetailCtrl controller', function () {
+    expect(EmployeeDetailCtrl).toBeDefined();
+  });
 
-            scope = $rootScope.$new();
+  it('should have correct css class',
+    inject( function($controller, $rootScope) {
+      scope = $rootScope.$new();
 
-            controller = $controller('EmployeeDetailCtrl', {
-                $scope: scope,
-                $routeParams:  { 'id' : 1 }
-            });
+      controller = $controller('EmployeeDetailCtrl', {
+        $scope: scope
+      });
 
-            expect(scope.routeId).toEqual(1);
-        })
-    );
+      expect(scope.classChange(24)).toBe('progress-bar progress-bar-danger');
+      expect(scope.classChange(74)).toBe('progress-bar progress-bar-warning');
+      expect(scope.classChange(75)).toBe('progress-bar progress-bar-success');
+      expect(scope.classChange(24)).not.toBe('progress-bar progress-bar-success');
+      expect(scope.classChange(74)).not.toBe('progress-bar progress-bar-success');
+      expect(scope.classChange(75)).not.toBe('progress-bar progress-bar-danger');
+    })
+  );
+
+  it('should take id as specified',
+    inject( function($controller, $rootScope) {
+      scope = $rootScope.$new();
+
+      controller = $controller('EmployeeDetailCtrl', {
+        $scope: scope,
+        $routeParams:  { 'id' : 1 }
+      });
+
+      expect(scope.routeId).toEqual(1);
+    })
+  );
 });
