802a51f7b81e662e0cbbfd2b68c877382941cd3e
bmehler <bernhard.mehler@gmail.com>
2015-08-16 11:23:34 +0000

    changed style

--- public/module/employee/controller/EmployeeListController.js
+++ public/module/employee/controller/EmployeeListController.js
@@ -1,26 +1,26 @@
 angular
-      .module('employee', [
-        'employee.config',
-      ])
-      .controller('EmployeeListCtrl', EmployeeListCtrl);
+  .module('employee', [
+    'employee.config',
+  ])
+  .controller('EmployeeListCtrl', EmployeeListCtrl);
 
 EmployeeListCtrl.$inject = ['$scope', 'EmployeeService'];
 
 function EmployeeListCtrl ($scope, EmployeeService) {
-    var employee = this;
-    employee.employees = [ ];
-    employee.orderProp = 'name';
+  var employee = this;
+  employee.employees = [ ];
+  employee.orderProp = 'name';
 
-    getEmployees();
+  getEmployees();
 
-    function getEmployees () {
-        EmployeeService.getEmployee()
-              .success(function (data) {
-                    employee.employees = data;
-                    return employee.employees;
-              })
-              .error(function (error) {
-                  console.log('Unable to read data' + error);
-              });
-      };
+  function getEmployees () {
+    EmployeeService.getEmployee()
+      .success(function (data) {
+        employee.employees = data;
+        return employee.employees;
+      })
+      .error(function (error) {
+        console.log('Unable to read data' + error);
+      });
+  };
 };
